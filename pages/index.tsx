fa979ffc0e4b7cb072ba32243e975f67c06fd432
tanisha34x <tanisha34x@gmail.com>
2025-06-12 09:44:09 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,18 @@
 import { GetServerSideProps } from 'next';
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const referer = context.req.headers.referer || '';
-  const hasFBCLID = 'fbclid' in context.query;
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const referer = ctx.req.headers.referer || '';
+  const hasFB = 'fbclid' in ctx.query;
 
-  if (referer.includes('facebook.com') || hasFBCLID) {
+  if (referer.includes('facebook.com') || hasFB) {
     return {
-      redirect: {
-        destination: 'https://tapatap.com/',
-        permanent: false,
-      },
+      redirect: { destination: 'https://tapatap.com/', permanent: false },
     };
   }
 
-  return {
-    props: {}, // will be passed to the page component as props
-  };
+  return { props: {} };
 };
 
 export default function Home() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Welcome to My Next.js App!</h1>
-      <p>This is the homepage.</p>
-    </div>
-  );
+  return <h1>Welcome to My Next.js App!</h1>;
 }
